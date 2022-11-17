19e33c758f062809d119bcda8cf75167afea3d13
Paulo Henrique Luiz Pereira <paulo0147wp@outlook.com>
2022-11-17 15:55:38 -0300

    gray_l_co_o_matrix-prop

--- aumento_de_dados.py
+++ aumento_de_dados.py
@@ -18,7 +18,6 @@ def espelhamento_horz(caminho_img, novo_caminho):
   #Salvando a imagem invertida horizontalmente
   cv2.imwrite(caminho_img_horz , horizontal_img)
 
-
 def histograma_equal(caminho_img, novo_caminho):
   #lendo a imagem e realizando a equalização do histograma 
   img = cv2.imread(caminho_img,0)
@@ -28,29 +27,29 @@ def histograma_equal(caminho_img, novo_caminho):
   nome_img = caminho_img[caminho_img.rfind('/') + 1 : caminho_img.rfind('.')]
   formato_img = caminho_img[caminho_img.rfind('.') : len(caminho_img)]
   caminho_h_equ = novo_caminho + nome_img + '_h_equ' + formato_img
+  print(caminho_h_equ)
 
-  #Salvando a imagem invertida horizontalmente
+  #Salvando a imagem com o histograma equalizado
   cv2.imwrite(caminho_h_equ , img_h_equ)
-
 
 def aumentoDados():
   for i in range(0, 5):
     #configuramos o diretório base onde está as imagens das 5 classes
-    diretorio_base = "../testes"
+    diretorio_base = "../test"
     #a cada iteração do for muda-se a classe das imagens para serem feitas o aumento de dados
     diretorio_atual = diretorio_base + '/' + str(i) + '/'
     #obtem o caminho completo do diretório atual
     diretorio = os.path.abspath(diretorio_atual)
-
+    '''
     #para cada imagem na pasta será feito a equalizacao do histograma e o espelhamento horizontal
     for img in Path(diretorio).iterdir():
       if img.is_file():
         caminho_completo = diretorio + '/' + img.name
-        espelhamento_horz(caminho_completo,diretorio_atual)
+        #espelhamento_horz(caminho_completo,diretorio_atual)
         histograma_equal(caminho_completo,diretorio_atual)
-    
+ 
     #usado caso queiramos criar novos diretorios para cada tipo de aumento de dados
-    '''
+
     novo_diretorio = os.path.join(diretorio_base, str(i) + '_esp_horz/')
     if not os.path.exists(novo_diretorio):
       os.makedirs(novo_diretorio)
@@ -58,6 +57,7 @@ def aumentoDados():
       if img.is_file():
         caminho_completo = diretorio + '/' + img.name
         espelhamento_horz(caminho_completo,novo_diretorio)
+
 
     novo_diretorio = os.path.join(diretorio_base, str(i) + '_h_equ/')
     if not os.path.exists(novo_diretorio):
@@ -67,6 +67,3 @@ def aumentoDados():
         caminho_completo = diretorio + '/' + img.name
         histograma_equal(caminho_completo,novo_diretorio)
     '''
-
-
-aumentoDados()
---
+++ classificador_raso.py
@@ -0,0 +1,67 @@
+import math
+import cv2
+import correlacao_cruzada as cc
+import aumento_de_dados as ad
+from skimage.feature import graycomatrix, graycoprops
+from pathlib import Path
+import os
+import numpy as np
+
+
+def pre_processamento():
+  for i in range(0, 5):
+    #configuramos o diretório base onde está as imagens das 5 classes
+    diretorio_base = "../test"
+    diretorio_class_raso = "../img_classf_raso"
+    #a cada iteração do for muda-se a classe das imagens para serem feitas o aumento de dados
+    diretorio_atual = diretorio_base + '/' + str(i) + '/'
+    #obtem o caminho completo do diretório atual
+    diretorio = os.path.abspath(diretorio_atual)
+    #confirgura o novo diretório que será salva as imagens com o histograma equaliazado para o classificador raso
+    novo_diretorio = os.path.join(diretorio_class_raso, str(i) + '/')
+    if not os.path.exists(novo_diretorio):
+      os.makedirs(novo_diretorio)
+    for img in Path(diretorio).iterdir():
+      if img.is_file():
+        caminho_completo = diretorio + '/' + img.name
+        ad.histograma_equal(caminho_completo,novo_diretorio)
+
+
+def gray_l_co_o_matix():
+  #propriedades que serão calculadas usando a matriz de co-ocorrencia de níveis de cinza
+  propriedades = ['contrast', 'dissimilarity', 'homogeneity', 'energy', 'correlation', 'ASM']
+  prop_imgs = []
+
+  for i in range(0, 5):
+    #configuramos o diretório base onde está as imagens das 5 classes
+    diretorio_class_raso = "../img_classf_raso"
+    #a cada iteração do for muda-se a classe das imagens para o calculo da matriz de co-ocorrencia de níveis de cinza
+    diretorio_atual = diretorio_class_raso + '/' + str(i) + '/'
+    #obtem o caminho completo do diretório atual
+    diretorio = os.path.abspath(diretorio_atual)
+
+    #percorre cada imagem do diretório e faz o caluclo da matriz, e da matriz tira cada uma das propriedades
+    for img in Path(diretorio).iterdir():
+      if img.is_file():
+        imagem = cv2.imread(diretorio + '/' + img.name,0)
+        prop_img = []
+        #calcula a matriz de co-ocorrencia de niveis de cinza
+        glcm = graycomatrix(imagem, distances = [5], angles = [0, np.pi/4, np.pi/2, 3*np.pi/4], 
+                            levels = 256, symmetric = True, normed = True)
+        #calcula todas as propiedades utilizando a matriz de co-ocorrencia de niveis de cinza
+        glcm_props = [prop for propriedade in propriedades 
+                            for prop in graycoprops(glcm, propriedade)[0]]
+        for item in glcm_props:
+          #coloca todos os valores das propriedades em uma lista
+          prop_img.append(item)
+        prop_img.append(img.name)
+        #coloca todos os valores das propriedades das imagens 
+        #em uma lista que conterá a propriedade de todas as imagens do dataset
+        prop_imgs.append(prop_img)
+
+  return prop_imgs
+
+if __name__=='__main__':
+  #pre_processamento()
+  carat = gray_l_co_o_matix()
+  print(carat)
